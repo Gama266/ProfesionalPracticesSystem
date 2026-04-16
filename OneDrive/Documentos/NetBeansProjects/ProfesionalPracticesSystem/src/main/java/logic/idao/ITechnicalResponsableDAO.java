afacf1481ba7aa161e59676c48c612ab8d926c9c
Gama266 <gamalielpro777@gmail.com>
2026-04-16 10:43:36 -0600

    FEAT:Se agregaron las idao

---
+++ OneDrive/Documentos/NetBeansProjects/ProfesionalPracticesSystem/src/main/java/logic/idao/ITechnicalResponsableDAO.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package logic.idao;
+
+/**
+ *
+ * @author gamal
+ */
+public interface ITechnicalResponsableDAO {
+    
+    
+}
